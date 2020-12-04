c71db27b9f34e9fa9664d31743d3988162bee2be
Phéna Proxima <adam@phenaproxima.net>
2020-12-04 11:17:28 -0500

    Refactor install.sh to actually use real ORCA stuff.

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -14,32 +14,27 @@ cd "$(dirname "$0")"
 # Reuse ORCA's own includes.
 source ../../../orca/bin/travis/_includes.sh
 
-CURRENT_RECOMMENDED="~9.1.0"
-CURRENT_DEV="9.1.x-dev"
-NEXT_DEV="9.2.x-dev"
-
-case "$ORCA_JOB" in
-  "DEPRECATED_CODE_SCAN") orca debug:packages; eval "orca fixture:init -f --sut=$ORCA_SUT_NAME --sut-only --no-site-install" ;;
-  "ISOLATED_RECOMMENDED") orca debug:packages "$CURRENT_RECOMMENDED"; eval "orca fixture:init -f --sut=$ORCA_SUT_NAME --sut-only --core=$CURRENT_RECOMMENDED --profile=$ORCA_FIXTURE_PROFILE --project-template=$ORCA_FIXTURE_PROJECT_TEMPLATE" ;;
-  "INTEGRATED_RECOMMENDED") orca debug:packages "$CURRENT_RECOMMENDED"; eval "orca fixture:init -f --sut=$ORCA_SUT_NAME --core=$CURRENT_RECOMMENDED --profile=$ORCA_FIXTURE_PROFILE --project-template=$ORCA_FIXTURE_PROJECT_TEMPLATE" ;;
-  # "CORE_PREVIOUS" is never used.
-  "ISOLATED_DEV") orca debug:packages "$CURRENT_DEV"; eval "orca fixture:init -f --sut=$ORCA_SUT_NAME --sut-only --core=$CURRENT_DEV --dev --profile=$ORCA_FIXTURE_PROFILE --project-template=$ORCA_FIXTURE_PROJECT_TEMPLATE" ;;
-  "INTEGRATED_DEV") orca debug:packages "$CURRENT_DEV"; eval "orca fixture:init -f --sut=$ORCA_SUT_NAME --core=$CURRENT_DEV --dev --profile=$ORCA_FIXTURE_PROFILE --project-template=$ORCA_FIXTURE_PROJECT_TEMPLATE" ;;
-  "CORE_NEXT") orca debug:packages "$NEXT_DEV"; eval "orca fixture:init -f --sut=$ORCA_SUT_NAME --core=$NEXT_DEV --dev --profile=$ORCA_FIXTURE_PROFILE --project-template=$ORCA_FIXTURE_PROJECT_TEMPLATE" ;;
-esac
-
-if [ -d "$ORCA_FIXTURE_DIR" ]; then
-  composer -d $ORCA_FIXTURE_DIR require phpspec/prophecy-phpunit:^2
+# When testing the SUT in isolation using dev package versions, treat the
+# components as part of the SUT, to be installed in an isolated (SUT-only)
+# fixture.
+if [[ "$ORCA_JOB" == "ISOLATED_DEV" ]]; then
+  export ORCA_PACKAGES_CONFIG=../headless_lightning/tests/packages.yml
+  orca fixture:init -f --sut="$ORCA_SUT_NAME" --dev --profile=headless_lightning
+else
+  # Run ORCA's standard installation script.
+  ../../../orca/bin/travis/install.sh
 fi
 
-# Upgrade test special case:
+# If there is no fixture, there's nothing else for us to do.
+[[ -d "$ORCA_FIXTURE_DIR" ]] || exit 0
+
+# Add testing dependencies.
+composer -d $ORCA_FIXTURE_DIR require phpspec/prophecy-phpunit:^2
 
 # Exit early if no DB fixture is specified.
 [[ "$DB_FIXTURE" ]] || exit 0
 
-orca fixture:init --force --sut "$ORCA_SUT_NAME" --sut-only --no-site-install --core="$CURRENT_DEV" --project-template=acquia/drupal-recommended-project
-
-cd "$ORCA_FIXTURE_DIR/docroot" || exit 0
+cd "$ORCA_FIXTURE_DIR/docroot"
 
 # Ensure the files directory exists so that the default user avatar can be
 # copied into it.
